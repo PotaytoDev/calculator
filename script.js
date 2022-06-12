4c4ba98d52378d4a29f3d0379b1e601e83176c8e
Potayto <p.o.tayto@outlook.com>
2022-06-11 17:37:22 -0700

    Create function operate
    This helper function determines what operator function to call based
    on the arguments passed to it.

--- script.js
+++ script.js
@@ -17,3 +17,29 @@ function divide(firstOperand, secondOperand)
 {
     return firstOperand / secondOperand;
 }
+
+function operate(operator, firstOperand, secondOperand)
+{
+    let result = 0;
+
+    switch (operator)
+    {
+        case '+':
+            result = add(firstOperand, secondOperand);
+            break;
+        case '-':
+            result = subtract(firstOperand, secondOperand);
+            break;
+        case '*':
+            result = multiply(firstOperand, secondOperand);
+            break;
+        case '/':
+            result = divide(firstOperand, secondOperand);
+            break;
+        default:
+            console.log("Something went wrong!");
+            break;
+    }
+
+    return result;
+}
